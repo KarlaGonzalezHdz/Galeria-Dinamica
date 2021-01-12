86e77798a24cbfc71de5e7ecf8914c9898d9619d
Karla GH <karlahdz.98@outlook.com>
2021-01-12 11:06:42 -0600

    estrutura del proyecto y realizacion del index

---
+++ views/index.view.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galeria</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">Mi Galeria Dinamica</h1>
+        </div>
+    </header>
+    <section class="fotos">
+        <div class="contenedor">
+            <div class="thumb">
+                <a href="">
+                    <img src="imagenes/1.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="">
+                    <img src="imagenes/3.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="">
+                    <img src="imagenes/4.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="">
+                    <img src="imagenes/5.jpg" alt="">
+                </a>
+            </div>
+
+            <div class="paginacion">
+                <a href="" class="izquierda"><i class="fa fa-long-arrow-left"></i> Pagina Anterior</a>
+                <a href="" class="derecha">Pagina Siguiente <i class="fa fa-long-arrow-right"></i></a>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+    <p class="copyright">Galeria creada por Karla GH</p>
+    </footer>
+
+</body>
+
+</html>
