dcae3093bd3148bdbe76422e73aabfe82a4a12df
Karla GH <karlahdz.98@outlook.com>
2021-01-12 12:52:26 -0600

    Conexion a la base de datos, traer los datos y mostrar en la galeria

--- views/subir.view.php
+++ views/subir.view.php
@@ -25,6 +25,9 @@
         <input type="text" id="titulo" name="titulo">
         <label for="texto">Descripcion:</label>
         <textarea name="texto" id="texto" placeholder="Ingresa una descripcion"></textarea>
+        <?php if(isset($error)): ?>
+            <p class="error"> <?php echo $error ;?></p>
+        <?php endif;?>
         <input type="submit" class="submit" value="subir foto">
         </form>
     </div>
